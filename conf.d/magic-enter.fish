b4dd5a6b8aa52c95ab1ea70759c20c51d438b7c0
mattmc3 <mattmc3@gmail.com>
2021-11-23 22:00:59 -0500

    Suppress autosuggestion

--- conf.d/magic-enter.fish
+++ conf.d/magic-enter.fish
@@ -16,6 +16,7 @@ function magic-enter
     set -l cmd (commandline)
     if test -z "$cmd"
         commandline -r (magic-enter-cmd)
+        commandline -f suppress-autosuggestion
     end
     commandline -f execute
 end
